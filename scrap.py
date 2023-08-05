93c69835f9e7ff9746cdf2adabc5fcc19f1cb654
Your Name <you@example.com>
2023-08-05 08:24:24 +0530

    Web scrapping is detailed:Reviews,Rating,Comment,Description

--- scrap.py
+++ scrap.py
@@ -2,8 +2,8 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import logging
-
-flipcart_url = "https://www.flipkart.com/search?q=" + "iphone12pro"
+pname=input(str("Enter Product Name:"))
+flipcart_url = f"https://www.flipkart.com/search?q={pname}" 
 print(flipcart_url)
 
 urlClient=urlopen(flipcart_url)
@@ -13,17 +13,38 @@ flipcart_page=urlClient.read()
 flipcart_html=bs(flipcart_page,'html.parser')
 
 bigbox=flipcart_html.findAll("div",{"class": "_1AtVbE col-12-12"})
-print(len(bigbox))
+#print(len(bigbox))
 
 del bigbox[0:2]
 del bigbox[-3:]
 
 
-for i in range(len(bigbox)):
-    print("https://www.flipkart.com"+bigbox[i].div.div.div.a['href'])
-    print('\n')
+#for i in range(len(bigbox)):
+productlink="https://www.flipkart.com"+bigbox[3].div.div.div.a['href']
+print(productlink)
 
-print(len(bigbox))  
+product_req=requests.get(productlink)
 
+product_html=bs(product_req.text,'html.parser')
+#print(product_req.text+"\n")
 
+product_box=product_html.findAll('div',{'class':'_16PBlm'})
+#print((product_box))
+del product_box[-1:]
+allreviewes=[]
+for i in product_box:
+    # print("Reviewer Name:"+i.div.find('p',{'class':'_2sc7ZR _2V5EHH'}).text)
+    # print("Rating:"+i.div.div.div.div.text)
+    # print("Comment:"+i.div.div.div.p.text)
+    # print("Discription:"+i.div.div.find_all('div',{"class":""}))
+    # print("=================================")
+    reviews={
+        "Reviewer Name": i.div.find('p',{'class':'_2sc7ZR _2V5EHH'}).text,
+        "Rating":i.div.div.div.div.text,
+        "Comment":i.div.div.div.p.text,
+        "Discription":i.div.div.find('div',{"class":""}).text
+    }
+    allreviewes.append(reviews)
+    
 
+print(allreviewes)
